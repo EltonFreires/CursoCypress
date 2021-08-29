67e80625b2dba53adbdb5bded836472133577010
Elton Freires <elton.freires@gmail.com>
2021-08-29 16:14:19 -0300

    aula 03, 04 e 05

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,5 +1,24 @@
 describe("Tickets", () => {
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
-    it("has 'ticketbox' header's heading", () => {});
-    
+
+    it("fill all the text input fields", () => {
+        const firstName = "Ana";
+        const lastName = "Maria";
+        cy.get("#first-name").type(firstName);
+        cy.get("#last-name").type(lastName);
+        cy.get("#email").type("fulano@fulano.com");
+        cy.get("#requests").type("PCD");
+        cy.get("#signature").type(`${firstName} ${lastName}`);
+    });
+
+    it("Select two tickets", () => {
+        cy.get("#ticket-quantity").select("2");
+    });
+
+    it("select 'vip' ticket type", () => {
+        cy.get("#vip").check();
+    });
+
+    // it("has 'ticketbox' header's heading", () => {});
+
 });
