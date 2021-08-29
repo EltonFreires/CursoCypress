0e4ff2ce8ae47893e63411552f9dd28161d5861d
Elton Freires <elton.freires@gmail.com>
2021-08-29 11:23:00 -0300

    aula 2 primeiro commit

--- cypress/integration/tickets.spec.js
+++ cypress/integration/tickets.spec.js
@@ -1,4 +1,5 @@
 describe("Tickets", () => {
     beforeEach(() => cy.visit("https://ticket-box.s3.eu-central-1.amazonaws.com/index.html"));
     it("has 'ticketbox' header's heading", () => {});
+    
 });
